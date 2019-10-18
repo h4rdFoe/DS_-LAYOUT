a52cd54d5aebf5c51cad7b9f25f750a9d2eb6967
h4rdFoe <h4rdfoe@gmail.com>
2019-10-18 10:29:53 +0300

    v1.0
    Первый залив

---
+++ js/carta_osm.js
@@ -0,0 +1,94 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.dropdown-trigger');
+    var instances = M.Dropdown.init(elems,{
+      constrainWidth:false
+    });
+  });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tooltipped');
+  var instances = M.Tooltip.init(elems,{
+    outDuration: 0
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#time');
+  var instances = M.Tooltip.init(elems,{
+    outDuration: 0,
+    margin: -30
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#time1');
+  var instances = M.Tooltip.init(elems,{
+    outDuration: 0,
+    margin: -11
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.modal');
+  var instances = M.Modal.init(elems)
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('#error_save');
+  var instances = M.Modal.init(elems, {
+    startingTop: '35%',
+    endingTop: '35%'
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.tabs');
+  var instance = M.Tabs.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.collapsible');
+  var instances = M.Collapsible.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+$(function() {
+  $('#hide-right-info').click(function() {
+    $('#right-info').toggleClass('scale-out absol');
+    $('#main-info').toggleClass('push-card');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  var instances = M.FloatingActionButton.init(elems, {
+    direction: 'top',
+    hoverEnabled: false
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.chips');
+  var instances = M.Chips.init(elems, {
+    placeholder: 'начните ввод'
+  });
+});
---
+++ js/search.js
@@ -0,0 +1,106 @@
+$(function() {
+    var chip = {
+      tag: 'chip content',
+      image: '', //optional
+    };
+    $('#diagnoz').chips({
+      placeholder: 'начните вводить диагноз',
+      secondaryPlaceholder: 'Диагноз',
+      autocompleteOptions: {
+        data: {
+          'A00 Холера': null,
+          'С00 Злокачественное новообразование губы': null,
+          'В03 Оспа': null
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+  });
+
+  $(function() {
+    var chip = {
+      tag: 'chip content',
+      image: '', //optional
+    };
+    $('#diagnoz1').chips({
+      placeholder: 'начните вводить диагноз',
+      secondaryPlaceholder: 'Диагноз',
+      autocompleteOptions: {
+        data: {
+          'A00 Холера': null,
+          'С00 Злокачественное новообразование губы': null,
+          'В03 Оспа': null
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+  });
+
+  $(function() {
+    var chip = {
+      tag: 'chip content',
+      image: '', //optional
+    };
+    $('#bolezni').chips({
+      placeholder: 'Начните ввод заболевания',
+      secondaryPlaceholder: 'Добавить заболевание',
+      autocompleteOptions: {
+        data: {
+          'сахарный диабет': null,
+          'туберкулез': null,
+          'эбола': null
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+  });
+
+  $(function() {
+    var chip = {
+      tag: 'chip content',
+      image: '', //optional
+    };
+    $('#vaccine').chips({
+      placeholder: 'Начните ввод наименования вакцины',
+      secondaryPlaceholder: 'Добавить вакцину',
+      autocompleteOptions: {
+        data: {
+          'БЦЖ - R1': null,
+          'Полиомиепит - V2': null,
+          'эбола - A1': null
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+  });
+
+  $(function() {
+    var chip = {
+      tag: 'chip content',
+      image: '', //optional
+    };
+    $('#CMO').chips({
+      placeholder: 'Страховая компания',
+      secondaryPlaceholder: 'Страховая компания',
+      autocompleteOptions: {
+        data: {
+          'АКЦИОНЕРНОЕ ОБЩЕСТВО "СТРАХОВАЯ КОМПАНИЯ "СОГАЗ-МЕД" (1002) - Республика Адыгея': null,
+          'ФИЛИАЛ АКЦИОНЕРНОГО ОБЩЕСТВА ВТБ МЕДИЦИНСКОЕ СТРАХОВАНИЕ В РЕСПУБЛИКЕ АДЫГЕЯ (1001) - Республика Адыгея': null,
+          'ФИЛИАЛ АО ВТБ МЕДИЦИНСКОЕ СТРАХОВАНИЕ В РЕСПУБЛИКЕ БАШКОРТОСТАН (2013) - Республика Башкортостан': null
+        },
+        limit: Infinity,
+        minLength: 1
+      }
+    });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      direction: 'left'
+    });
+  });
